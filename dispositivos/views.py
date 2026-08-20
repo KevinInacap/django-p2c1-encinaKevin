4f8a3b8fe1b4f7cba6fd8efdedf40006458fb76f
Laboratorio Computación N° 9 <claseslab9@inacap.cl>
2026-08-20 10:09:30 -0400

    chore: preparar proyecto Django

---
+++ dispositivos/views.py
@@ -0,0 +1,23 @@
+from django.shortcuts import render
+
+# Create your views here.
+
+# dispositivos/views.py
+from django.http import HttpResponse
+def inicio(request):
+ return HttpResponse(
+"<h1>EcoEnergy</h1>"
+"<p>Back End en funcionamiento</p>"
+)
+
+def dispositivos_zona(request, zona_id):
+    if zona_id != 1:
+        return HttpResponse(
+        "Zona no encontrada", status=404
+        )
+    return   HttpResponse   (  
+    f"Dispositivos de la zona {zona_id}"
+    )
+
+
+
